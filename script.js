05c96c91db061ea206ee80826d2146b4a33bf8ae
Puru33 <90132747+Puru33@users.noreply.github.com>
2026-01-01 17:05:06 +0530

    previous code removed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-=======
+// <<<<<<< HEAD
+// =======
 let humanScore = 0;
 let computerScore = 0;
 
@@ -14,7 +14,7 @@ const scissorsBtn = document.querySelector("#scissors");
 
 const resetBtn = document.querySelector("#reset");
 
->>>>>>> rps-ui
+// >>>>>>> rps-ui
 function getComputerChoice() {
   let ramdomNumber = Math.floor(Math.random() * 3);
   if (ramdomNumber === 0) {
@@ -26,75 +26,9 @@ function getComputerChoice() {
   }
 }
 
-<<<<<<< HEAD
-function getHumanChoice() {
-  const userInput = prompt("enter rock, paper, scissors:");
-  const choice = userInput.toLowerCase().trim();
-
-  if (choice === "rock" || choice === "paper" || choice === "scissors") {
-    return choice;
-  } else {
-    alert("Invalid choice! please enter rock, paper or scissors ");
-  }
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice === computerChoice) {
-      console.log("It's a tie!");
-    } else if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      console.log(`You win! Computer choose: ${computerChoice} `);
-
-      humanScore++;
-    } else {
-      console.log(`Computer Win! Computer choose: ${computerChoice}`);
-
-      computerScore++;
-    }
-    console.log(`score > Human: ${humanScore}, Computer: ${computerScore}`);
-  }
-
-  console.log("Round 1");
-  playRound(getHumanChoice(), getComputerChoice());
-
-  console.log("Round 2");
-  playRound(getHumanChoice(), getComputerChoice());
-
-  console.log("Round 3");
-  playRound(getHumanChoice(), getComputerChoice());
-
-  console.log("Round 4");
-  playRound(getHumanChoice(), getComputerChoice());
-
-  console.log("Round 5");
-  playRound(getHumanChoice(), getComputerChoice());
-
-  console.log("Game Over");
-  if (humanScore > computerScore) {
-    console.log("You won the game");
-  } else if (computerScore > humanScore) {
-    console.log("computer won the game");
-  } else {
-    console.log("It's Draw overall");
-  }
-}
-
-playGame();
-
-// const humenSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humenSelection, computerSelection);
-=======
+// <<<<<<<
+//  HEAD
+// =======
 function playRound(humanChoice) {
   if (humanScore === 5 || computerScore == 5) {
     return;
@@ -177,4 +111,4 @@ rockBtn.addEventListener("click", () => playRound("rock"));
 paperBtn.addEventListener("click", () => playRound("paper"));
 scissorsBtn.addEventListener("click", () => playRound("scissors"));
 
->>>>>>> rps-ui
+// >>>>>>> rps-ui
